a53b4a09f521680799710d84e238f26392c9c555
Erwin Rooijakkers <erwin@rooijakkers.software>
2018-02-07 22:23:43 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-iota "0.0.3"
+(defproject cljs-iota "0.0.4"
   :description    "ClojureScript API for IOTA JavaScript API"
 
   :url            "https://github.com/transducer/cljs-iota"
