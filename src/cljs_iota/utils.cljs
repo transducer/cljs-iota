3748aaeaa2921e709f40bf8e5f36a4d6fb0bb737
Erwin Rooijakkers <erwin@rooijakkers.software>
2018-02-23 16:16:22 +0100

    Rest of utils functions

--- src/cljs_iota/utils.cljs
+++ src/cljs_iota/utils.cljs
@@ -73,8 +73,146 @@
 
   Arguments:
   iota - IOTA client instance
-  address: string | coll 90-trytes address. Either string or a coll.
+  address - string | coll 90-trytes address. Either string or a coll.
 
   Returns string | coll - returns the 81-tryte address(es)"
   [iota address]
   (js-utils/js-apply (utils iota) "noChecksum" [address]))
+
+
+(defn valid-checksum?
+  "Takes an 90-trytes checksummed address and returns a true / false if it is
+  valid.
+
+  Arguments:
+  iota - IOTA client instance
+  address-with-checksum - String 90-trytes address
+
+  Returns true or false whether the checksum is valid or not."
+  [iota address-with-checksum]
+  (js-utils/js-apply (utils iota) "isValidChecksum" [address-with-checksum]))
+
+
+(defn transaction-object
+  "Converts the trytes of a transaction into its transaction object.
+
+  iota - IOTA client instance
+  trytes: string 2673-trytes of a transaction
+
+  Returns transaction map."
+  [iota trytes]
+  (js-utils/js-apply (utils iota) "transactionObject" [trytes]))
+
+
+(defn transaction-trytes
+  "Converts a valid transaction object into trytes. Please refer to
+  https://domschiener.gitbooks.io/iota-guide/content/chapter1/transactions-and-bundles.html
+  for more information what a valid transaction object looks like.
+
+  Arguments:
+  iota - IOTA client instance
+  transaction-object: Object valid transaction object
+
+  Returns converted trytes of transaction."
+  [iota transaction]
+  (js-utils/js-apply (utils iota) "transactionTrytes" [transaction]))
+
+
+(defn categorize-transfers
+  "Categorizes a list of transfers into sent and received. It is important to
+  note that zero value transfers (which are, for example, used for storing
+  addresses in the Tangle), are seen as received in this function.
+
+  Arguments:
+  iota - IOTA client instance
+  transfers - coll A coll of bundles. Basically is an array, of arrays (bundles),
+                   as is returned from `get-transfers` or `get-account-data`
+  addresses - coll of addresses that belong to you. With these addresses as
+              input, it's determined whether it's a sent or a receive transaction.
+              Therefore make sure that these addresses actually belong to you.
+
+  Returns map of the transfers categorized into :sent and :received"
+  [iota transfers addresses]
+  (js-utils/js-apply (utils iota) "categorizeTransfers" [transfers addresses]))
+
+
+(defn to-trytes
+  "Converts ASCII characters into trytes according to our encoding schema (read
+  the source code for more info as to how it works). Currently only works with
+  valid ASCII characters. As such, if you provide invalid characters the
+  function will return `nil`.
+
+  Arguments:
+  iota - IOTA client instance
+  input - String you want to convert into trytes. All non-string values
+          should be converted into strings first.
+
+  Returns string || nil - trytes, or nil in case you provided an invalid ASCII
+  character"
+  [iota input]
+  (js-utils/js-apply (utils iota) "toTrytes" [input]))
+
+
+(defn from-trytes
+  "Reverse of toTrytes.
+
+  Input
+  iota - IOTA client instance
+  trytes - string Trytes you want to convert to string
+
+  Returns string - converted string"
+  [iota input]
+  (js-utils/js-apply (utils iota) "fromTrytes" [input]))
+
+
+(defn extract-json
+  "This function takes a bundle as input and from the
+  `signature-message-fragments` extracts the JSON encoded data which was sent
+  with the transfer. This currently only works with the `to-trytes` and
+  `from-trytes` function that use the ASCII <-> Trytes encoding scheme. In case
+  there is no JSON data, or invalid one, this function will return nil.
+
+  Arguments:
+  iota - IOTA client instance
+  bundle - Array bundle from which you want to extract the JSON data.
+
+  Returns stringified JSON object which was extracted from the transactions."
+  [iota bundle]
+  (js-utils/js-apply (utils iota) "extractJson" [bundle]))
+
+
+(defn validate-signatures
+  "This function makes it possible for each of the co-signers in the
+  multi-signature to independently verify that a generated transaction with the
+  corresponding signatures of the co-signers is valid. This function is safe to
+  use and does not require any sharing of digests or key values.
+
+  Arguments:
+  iota - IOTA client instance
+  signed-bundle - coll signed bundle by all of the co-signers
+  input-address - string input address as provided to `initiate-transfer`
+
+  Returns bool - true / false"
+  [iota signed-bundle input-address]
+  (js-utils/js-apply (utils iota)
+                     "validateSignatures"
+                     [signed-bundle input-address]))
+
+
+(defn bundle?
+  "Checks if the provided bundle is valid. The provided bundle has to be
+  ordered tail (i.e. current-index: 0) first. A bundle is deemed valid if it has:
+
+  - Valid transaction structure
+  - Correct current-index, last-index and number of bundle transactions
+  - The sum of all value fields is 0
+  - The bundle hash is correct
+  - Valid signature
+
+  Arguments:
+  iota - IOTA client instance
+  bundle: Array bundle to test
+
+  Returns bool - true / false"
+  [iota bundle]
+  (js-utils/js-apply (utils iota) "isBundle" [bundle]))
--- test/cljs_iota/utils_test.cljs
+++ test/cljs_iota/utils_test.cljs
@@ -7,7 +7,8 @@
             [cljs-iota.test-utils :refer [contains-keys? test-async test-within]]
             [cljs-iota.utils :as iota-utils]
             [cljs.test :refer-macros [async deftest is testing]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [goog.array :as garray]))
 
 
 (def iota (iota/create-iota "http://localhost:14700"))
@@ -41,7 +42,145 @@
            [(str address1 checksum1) (str address2 checksum2)]))))
 
 
-(deftest no-checksum-test
-  (let [address "UGSNHG9ULXTTHADQZZJDIWQSAPHFIRSMHJFADTJRJCDAKGNCEEUPYAF99CXFEZQDXRNQVCL9MFKNNUBZWISZJXTWEC"]
-    (is (= (iota-utils/no-checksum iota address)
-           "UGSNHG9ULXTTHADQZZJDIWQSAPHFIRSMHJFADTJRJCDAKGNCEEUPYAF99CXFEZQDXRNQVCL9MFKNNUBZW"))))
+(deftest valid-checksum?-test
+  (let [address-with-checksum    "UGSNHG9ULXTTHADQZZJDIWQSAPHFIRSMHJFADTJRJCDAKGNCEEUPYAF99CXFEZQDXRNQVCL9MFKNNUBZWISZJXTWEC"
+        address-invalid-checksum "UGSNHG9ULXTTHADQZZJDIWQSAPHFIRSMHJFADTJRJCDAKGNCEEUPYAF99CXFEZQDXRNQVCL9MFKNNUBZWISZJXTWED"]
+    (is (iota-utils/valid-checksum? iota address-with-checksum))
+    (is (not (iota-utils/valid-checksum? iota address-invalid-checksum)))))
+
+
+;;; Transaction trytes from `iota-api/prepare-transfers` with address
+;;; "GPRGUPKQLWBFUVCJVVUMZZSYXMHLWYYV9GE9EGMMMYSQAZHUBSXQTAVFAXJFJWVUVRXNUCGJHJDVJKCOAODSDDKPKW"
+;;; and seed
+;;; "OAATQS9VQLSXCLDJVJJVYUGONXAXOFMJOZNSYWRZSWECMXAQQURHQBJNLD9IOFEPGZEPEMPXCIVRX9999"
+
+(def transaction-trytes
+  (string/join
+   ["9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "999999999999999999999999999999999999999999999999999999GPRGUPKQLWBFUVCJVVUMZZSYX"
+    "MHLWYYV9GE9EGMMMYSQAZHUBSXQTAVFAXJFJWVUVRXNUCGJHJDVJKCOA99999999999999999999999"
+    "9999PD9999999999999999999999999OEZZUYD99999999999999999999SOUPXIAXFXZYV9EWNTLGE"
+    "GLBLKSBJEHVONUQSCIHFSCUECWYDEFOAIWHFPKVONKB9GLUUCFFKUCKWEUDC9999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "9999999999999999999999999999999999999999999999999999999999999999999999999999999"
+    "999999999999999999999999999999999999999999999999999999999999999999"]))
+
+
+(deftest transaction-object-and-transaction-trytes-test
+  (let [transaction-object (iota-utils/transaction-object iota transaction-trytes)]
+
+    (testing "transaction-object"
+      (is (contains-keys? transaction-object
+                          :address
+                          :last-index
+                          :hash
+                          :attachment-timestamp
+                          :value
+                          :bundle
+                          :trunk-transaction
+                          :branch-transaction
+                          :signature-message-fragment
+                          :current-index
+                          :attachment-timestamp-upper-bound
+                          :tag
+                          :obsolete-tag
+                          :timestamp
+                          :nonce
+                          :attachment-timestamp-lower-bound)))
+
+    (testing "transaction-trytes"
+      (is (= (iota-utils/transaction-trytes iota transaction-object)
+             transaction-trytes)))))
+
+
+;;; Transfers returned from `iota-api/get-transfers` with seed
+;;; "PWWWAKUJYRHFLVDCIOFKAKBUYTPUXQMBYCYSB9WIFY9WCGNZKOH9DOECZEZURIGRTRXFKNUZONGFA9999"
+
+(def transfers
+  [{:address                          "GPRGUPKQLWBFUVCJVVUMZZSYXMHLWYYV9GE9EGMMMYSQAZHUBSXQTAVFAXJFJWVUVRXNUCGJHJDVJKCOA"
+    :last-index                       0
+    :hash                             "PWWWAKUJYRHFLVDCIOFKAKBUYTPUXQMBYCYSB9WIFY9WCGNZKOH9DOECZEZURIGRTRXFKNUZONGFA9999"
+    :attachment-timestamp             1518970638562
+    :value                            0
+    :bundle                           "SOUPXIAXFXZYV9EWNTLGEGLBLKSBJEHVONUQSCIHFSCUECWYDEFOAIWHFPKVONKB9GLUUCFFKUCKWEUDC"
+    :trunk-transaction                "VIBDGRFTQ9SQDVAVKYJSQCDX9NCXRPT9NLTENEYARZYQGPPDVFWS9EA9ZMPXLGFRLJEGBYBGV9PGZ9999"
+    :branch-transaction               "GKVQLV9ORKHJACTYHVZHFTCBJOCMVAJHGYKQ9HVAEE9WVMJS9MCVKOFKEMDFIMJZFJMCEEFXBNAWZ9999"
+    :signature-message-fragment       (str "ODGABCCCKBCCDCBCGADBEAGABCDCMBMBOBBCBCGAQD" (apply str (repeat 2145 "9")))
+    :current-index                    0
+    :attachment-timestamp-upper-bound 12
+    :tag                              "PD9999999999999999999999999"
+    :obsolete-tag                     "PD9999999999999999999999999"
+    :timestamp                        1517775207
+    :nonce                            "IEDZBBJENMHCTMNWUJTPDKDITBK"
+    :attachment-timestamp-lower-bound 0}])
+
+
+(deftest categorize-transfers-test
+  (let [{:keys [sent received] :as categorized}
+        (iota-utils/categorize-transfers iota
+                                         [transfers]
+                                         (keep :address transfers))]
+    (is (empty? sent))
+    (is (= received [transfers]))))
+
+
+(deftest to-trytes-test
+  (testing "valid ASCII string"
+    (is (= "LBOBKBDCCCSBPBDCVB" (iota-utils/to-trytes iota "BEAUTIFUL"))))
+
+  (testing "something else"
+    (is (nil? (iota-utils/to-trytes iota 1)))))
+
+
+(deftest from-trytes-test
+  (testing "valid trytes"
+    (is (= "BEAUTIFUL" (iota-utils/from-trytes iota "LBOBKBDCCCSBPBDCVB"))))
+
+  (testing "invalid trytes"
+    (is (nil? (iota-utils/from-trytes iota "INVALID")))))
+
+
+(deftest extract-json-test
+  (is (= "{\"STATUS\": \"SUCCESS\"}"
+         (iota-utils/extract-json iota transfers))))
+
+
+(deftest validate-signatures-test
+  ;; TODO: true?
+  (is (false? (iota-utils/validate-signatures iota
+                                              transfers
+                                              (keep :address transfers)))))
+
+
+(deftest bundle?-test
+  (testing "valid bundle"
+    (is (true? (iota-utils/bundle? iota transfers))))
+
+  (testing "invalid bundle (without address)"
+    (is (false? (iota-utils/bundle? iota (map #(dissoc % :address) transfers))))))
